99735c4267bb2d7be0772e83ff9ae89779a28969
leaneleclercq <l_leclercq4@etu-webschoolfactory.fr>
2025-06-06 16:31:12 +0200

    "app.js" done

--- app_knex/app.js
+++ app_knex/app.js
@@ -2,9 +2,17 @@
 const express = require('express');
 const app = express();
 const userRoutes = require('./userRoutes');
+const annonceRoutes = require('./annonceRoutes');
+const propositionRoutes = require('./propositionRoutes');
 
 app.use(express.json());
 app.use('/', userRoutes); // Par exemple, utilisez '/api' comme préfixe pour toutes les routes des utilisateurs
+app.use('/', annonceRoutes);
+app.use('/', propositionRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur la page d\'accueil !')
+})
 
 const PORT = 80;
 app.listen(PORT, () => {
